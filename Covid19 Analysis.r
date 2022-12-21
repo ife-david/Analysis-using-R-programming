f15889559910eca2488c2efb89e71d700990169a
Ifeoluwa Omosowoeni <65132713+ife-david@users.noreply.github.com>
2022-12-21 01:31:41 +0100

    Update Covid19 Analysis.r

--- Covid19 Analysis.r
+++ Covid19 Analysis.r
@@ -2,20 +2,20 @@ rm(list =ls())#To remove all variables previously loaded
 
 library("Hmisc")
 
-data <- read.csv("~/Personal Development/Data Analytics/Projects/R/COVID19_line_list_data.csv")
+covid19_raw <- read.csv("~/Personal Development/Data Analytics/Projects/R/COVID19_line_list_data.csv")
 describe(data)
 
 #cleaned up data
-data$death_dummy <- as.integer(data$death != 0)
+covid19_raw$death_cleaned <- as.integer(covid19_raw$death != 0)
 
 #death rate
-sum(data$death_dummy)/nrow(data)
+sum(covid19_raw$death_cleaned)/nrow(covid19_raw)
 
 #Age
 #claim: The average age of those who die from covid is higher than those who survive
 
-dead <- subset(data, death_dummy == 1)
-alive <- subset(data, death_dummy == 0)
+dead <- subset(covid19_raw, death_cleaned == 1)
+alive <- subset(covid19_raw, death_cleaned == 0)
 
 mean(dead$age, na.rm = TRUE)
 mean(alive$age, na.rm = TRUE)
@@ -29,12 +29,12 @@ t.test(alive$age, dead$age, alternative = "two.sided", conf.level = 0.95)
 #Gender
 #claim: Gender has no effect.
 
-male <- subset(data, gender == "male")
-female <- subset(data, gender == "female")
-mean(male$death_dummy, na.rm = TRUE) #8.4%
-mean(female$death_dummy, na.rm = TRUE) #3.66%
+male <- subset(covid19_raw, gender == "male")
+female <- subset(covid19_raw, gender == "female")
+mean(male$death_cleaned, na.rm = TRUE) #8.4%
+mean(female$death_cleaned, na.rm = TRUE) #3.66%
 
 #is this statistically significant
-t.test(male$death_dummy, female$death_dummy, alternative = "two.sided", conf.level = 0.95)
+t.test(male$death_cleaned, female$death_cleaned, alternative = "two.sided", conf.level = 0.95)
 #The test showed a p value of 0.002105 which is far less than 0.05.Therefore we reject the null hypothesis
 #Conclusion - statistically significant
